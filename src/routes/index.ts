b8be3fe26306f1eeb706a953e1c784252faa2cf6
Bewizi <toluwalase.bewaji@yahoo.com>
2025-02-27 00:03:00 +0100

    added: added a route folder to navigate the links

---
+++ src/routes/index.ts
@@ -0,0 +1,49 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HeroSection from "../components/HeroSection.vue";
+import FeaturesSection from "../components/FeaturesSection.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "HeroSection",
+    component: HeroSection,
+  },
+  {
+    path: "/aboutsection",
+    name: "FeaturesSection",
+    component: FeaturesSection,
+  },
+  {
+    path: "/services",
+    name: "ServiceSection",
+    component: () => import("../components/CaseStudies.vue"),
+  },
+  {
+    path: "/technology",
+    name: "TechnologySection",
+    component: () => import("../components/TechnologiesSection.vue"),
+  },
+  {
+    path: "/howItWorks",
+    name: "HowItWorksSection",
+    component: () => import("../components/HowItWorks.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+
+  routes,
+  // scrool behavior
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return { el: to.hash, behavior: "smooth" };
+    } else if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
+});
+
+export default router;
