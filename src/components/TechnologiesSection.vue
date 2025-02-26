7f0c776833b3a7d4d8a6d711f40fcb78ab8662ca
Bewizi <toluwalase.bewaji@yahoo.com>
2025-02-26 13:46:07 +0100

    added TechnologiesSection Page, and its responsive on mobile table and desktop.

---
+++ src/components/TechnologiesSection.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup></script>
+
+<template>
+  <section class="py-20">
+    <div class="container mx-auto px-4">
+      <div class="text-center mb-12">
+        <div class="flex justify-center mb-4">
+          <div class="bg-[#FFA500] w-10 h-1 rounded-full mt-6 mr-2"></div>
+          <h2 class="text-3xl md:text-4xl font-bold mb-12">
+            Technologies We Use
+          </h2>
+          <div class="bg-[#FFA500] w-10 h-1 rounded-full mt-6 ml-2"></div>
+        </div>
+        <p class="font-bold font-Poppins text-xl">Our Development Tools</p>
+      </div>
+
+      <figure class="flex justify-center items-center">
+        <img alt="Technologies We Use" src="/technologies.gif" />
+      </figure>
+    </div>
+  </section>
+</template>
