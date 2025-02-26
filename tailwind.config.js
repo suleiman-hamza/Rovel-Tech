2533789b6d9fa32496ff2b9f98f00a3eefc3d593
Bewizi <toluwalase.bewaji@yahoo.com>
2025-02-26 13:53:08 +0100

    Initialzing commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0066FF",
+        secondary: "#001F3F",
+      },
+      fontFamily: {
+        Poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
