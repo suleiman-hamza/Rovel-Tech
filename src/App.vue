3768ebeafe26b035e24a511b6dfc9868f0786fd7
Bewizi <toluwalase.bewaji@yahoo.com>
2025-02-26 13:48:41 +0100

    Initialzing commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import Navbar from "./components/Navbar.vue";
+import HeroSection from "./components/HeroSection.vue";
+import FeaturesSection from "./components/FeaturesSection.vue";
+import CaseStudies from "./components/CaseStudies.vue";
+import TechnologiesSection from "./components/TechnologiesSection.vue";
+import HowItWorks from "./components/HowItWorks.vue";
+import WhyChooseUs from "./components/WhyChooseUs.vue";
+import IndustriesWeEmpower from "./components/IndustriesWeEmpower.vue";
+import VisionStatement from "./components/VisionStatement.vue";
+import Testimonials from "./components/Testimonials.vue";
+import ContactSection from "./components/ContactSection.vue";
+import FooterSection from "./components/FooterSection.vue";
+</script>
+
+<template>
+  <div class="min-h-screen bg-white">
+    <Navbar />
+    <HeroSection />
+    <FeaturesSection />
+    <CaseStudies />
+    <TechnologiesSection />
+    <HowItWorks />
+    <WhyChooseUs />
+    <IndustriesWeEmpower />
+    <VisionStatement />
+    <Testimonials />
+    <ContactSection />
+    <FooterSection />
+  </div>
+</template>
