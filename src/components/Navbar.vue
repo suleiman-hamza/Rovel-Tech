7476e9768844b60b513bb6bd2d01fd5b0a0adea7
Bewizi <toluwalase.bewaji@yahoo.com>
2025-02-26 13:29:41 +0100

    added the navbar, yet to work on the responsiveness

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup></script>
+
+<template>
+  <section class="container mx-auto">
+    <header class="py-8 flex justify-between items-center">
+      <figure>
+        <img alt="Logo" src="/public/images/Logo.svg" width="100px" />
+      </figure>
+      <nav class="flex items-center">
+        <ul class="flex space-x-8 text-lg font-Poppins font-bold">
+          <li>
+            <a href="" class="text-[#FFA500]">Home</a>
+          </li>
+          <li>
+            <a href="">About Us</a>
+          </li>
+          <li><a href="">Services</a></li>
+          <li><a href="">How it works</a></li>
+          <li><a href="">Why Choose Us</a></li>
+          <li><a href="">Testimonial</a></li>
+        </ul>
+        <div class="ml-12">
+          <button
+            class="bg-[#FFA500] text-white px-8 py-2 rounded-full font-bold"
+          >
+            Contact Us
+          </button>
+        </div>
+      </nav>
+    </header>
+  </section>
+</template>
+
+<style scoped></style>
