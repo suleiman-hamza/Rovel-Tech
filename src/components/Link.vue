9cafb7f7fe3f88d9376e5246e62b30c9c6fcbb1e
Bewizi <toluwalase.bewaji@yahoo.com>
2025-02-27 12:27:54 +0100

    Added: a link to my component folder so as to use it instead of router-link

---
+++ src/components/Link.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+// import { defineProps } from 'vue'
+import { RouterLink } from "vue-router";
+
+interface LinkProps {
+  to: string;
+  activeClass?: string;
+  exactActiveClass?: string;
+}
+
+defineProps<LinkProps>();
+</script>
+
+<template>
+  <RouterLink
+    :active-class="activeClass"
+    :exact-active-class="exactActiveClass"
+    :to="to"
+  >
+    <slot></slot>
+  </RouterLink>
+</template>
+
+<style scoped></style>
