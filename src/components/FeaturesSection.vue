72ad644a59f3229e27fd0bbeef0f821531630060
Bewizi <toluwalase.bewaji@yahoo.com>
2025-03-04 18:39:29 +0100

    added: font family Poppins to h3 elements

--- src/components/FeaturesSection.vue
+++ src/components/FeaturesSection.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup>
 import "animate.css";
-import {useIntersectionObserver} from "@vueuse/core";
+import { useIntersectionObserver } from "@vueuse/core";
 
-import {reactive, ref} from "vue";
+import { reactive, ref } from "vue";
 
 const features = reactive([
   {
@@ -42,11 +42,7 @@ useIntersectionObserver(
 </script>
 
 <template>
-  <section
-    id="about"
-    ref="aboutSectionRef"
-    class="py-20 animate__animated"
-  >
+  <section id="about" ref="aboutSectionRef" class="py-20 animate__animated">
     <div class="container mx-auto px-4">
       <div
         class="flex flex-col justify-between items-center md:flex-col lg:flex-row"
@@ -54,7 +50,9 @@ useIntersectionObserver(
         <div class="text-center md:text-left">
           <div class="flex justify-center">
             <div class="bg-[#FFA500] w-10 h-1 rounded-full mt-6 mr-2"></div>
-            <h1 class="text-4xl md:text-5xl font-bold font-PtSansCaption mb-8">About Us</h1>
+            <h1 class="text-4xl md:text-5xl font-bold font-PtSansCaption mb-8">
+              About Us
+            </h1>
             <div class="bg-[#FFA500] w-10 h-1 rounded-full mt-6 ml-2"></div>
           </div>
           <h3
@@ -73,7 +71,7 @@ useIntersectionObserver(
           </p>
         </div>
 
-<!--        Mobile View-->
+        <!--        Mobile View-->
         <div
           v-for="(feature, index) in features"
           :key="index"
@@ -89,14 +87,16 @@ useIntersectionObserver(
           </div>
         </div>
 
-<!--        Tablet and Mobile view-->
+        <!--        Tablet and Desktop view-->
 
         <div class="hidden md:grid md:grid-cols-3 mt-10 gap-0">
           <div
             class="text-center md:col-start-2 flex justify-center items-center"
           >
             <div class="bg-[#057CC3] w-[65%] p-6 text-white text-center">
-              <h3 class="text-2xl font-bold mt-4">{{ features[0].title }}</h3>
+              <h3 class="text-2xl font-bold mt-4 font-Poppins">
+                {{ features[0].title }}
+              </h3>
               <p class="font-Poppins">{{ features[0].description }}</p>
             </div>
           </div>
@@ -107,7 +107,9 @@ useIntersectionObserver(
             <div
               class="bg-[#057CC3] w-[50%] h-40 p-6 flex flex-col justify-center text-white"
             >
-              <h3 class="text-2xl font-bold mt-4">{{ features[1].title }}</h3>
+              <h3 class="text-2xl font-bold mt-4 font-Poppins">
+                {{ features[1].title }}
+              </h3>
               <p class="font-Poppins uppercase">
                 {{ features[1].description }}
               </p>
@@ -126,7 +128,9 @@ useIntersectionObserver(
             <div
               class="bg-[#057CC3] w-[50%] h-40 p-6 flex flex-col justify-center"
             >
-              <h3 class="text-2xl font-bold mt-4">{{ features[2].title }}</h3>
+              <h3 class="text-2xl font-bold mt-4 font-Poppins">
+                {{ features[2].title }}
+              </h3>
               <p class="font-Poppins uppercase">
                 {{ features[2].description }}
               </p>
@@ -137,7 +141,9 @@ useIntersectionObserver(
             class="text-center md:col-start-2 md:row-start-3 flex justify-center items-center"
           >
             <div class="bg-[#057CC3] w-[65%] p-6 text-white text-center">
-              <h3 class="text-2xl font-bold mt-4">{{ features[3].title }}</h3>
+              <h3 class="text-2xl font-bold mt-4 font-Poppins">
+                {{ features[3].title }}
+              </h3>
               <p class="font-Poppins">{{ features[3].description }}</p>
             </div>
           </div>
