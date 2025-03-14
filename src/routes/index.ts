dc54e00be560cf3e146d2656e4ac3c02ca07cd7f
Bewizi <toluwalase.bewaji@yahoo.com>
2025-03-14 12:46:26 +0100

    removed other routes, since i'm dynamically routing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,5 @@
 import {createRouter, createWebHistory} from "vue-router";
 import HeroSection from "../components/HeroSection.vue";
-import FeaturesSection from "../components/FeaturesSection.vue";
 
 const routes = [
   {
@@ -8,36 +7,7 @@ const routes = [
     name: "HeroSection",
     component: HeroSection,
   },
-  {
-    path: "/aboutsection",
-    name: "FeaturesSection",
-    component: FeaturesSection,
-  },
-  {
-    path: "/services",
-    name: "ServiceSection",
-    component: () => import("../components/CaseStudies.vue"),
-  },
-  {
-    path: "/technology",
-    name: "TechnologySection",
-    component: () => import("../components/TechnologiesSection.vue"),
-  },
-  {
-    path: "/howItWorks",
-    name: "HowItWorksSection",
-    component: () => import("../components/HowItWorks.vue"),
-  },
-  {
-    path: "/whyChooseUs",
-    name: "WhyChooseUsSection",
-    component: () => import("../components/WhyChooseUs.vue"),
-  },
-  {
-    path: "/testimonial",
-    name: "TestimonialSection",
-    component: () => import("../components/Testimonials.vue"),
-  },
+
 ];
 
 const router = createRouter({
