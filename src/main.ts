6dd95750cca5690bc759f5f5314895feb440c8be
Bewizi <toluwalase.bewaji@yahoo.com>
2025-03-14 11:52:24 +0100

    import primevue a vue package

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import { createApp } from "vue";
+import {createApp} from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./routes";
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura'
 
 const app = createApp(App);
 app.use(router);
+app.use(PrimeVue,{
+    theme:{
+        preset:Aura
+    }
+})
 app.mount("#app");
